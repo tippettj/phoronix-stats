38ec4cb20b615b66f5295bb5a774399035f1dd5d
tippettj <tippettj@gmail.com>
2021-02-18 22:10:55 -0800

    Extracted StyledTableCell from PTS Packages

---
+++ src/components/StyledTableCell.js
@@ -0,0 +1,24 @@
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+ const StyledTableCell = withStyles((theme) => ({
+    head: {
+      //backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+      padding: "0px",
+      backgroundColor: "#CCD1D1", //light grey
+
+    },
+    body: {
+      fontSize: 14,
+      padding:"2px",
+      marginLeft:"1.5em",
+      borderWidth: "2px",
+      borderStyle: "dotted",
+      borderColor: "#eff1f1",
+    }
+  }))(TableCell);
+
+  export default StyledTableCell;
+
+  
