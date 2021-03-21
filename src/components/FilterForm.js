8e6eb871046356f34e70ff96df2ec4fd74ef722d
tippettj <tippettj@gmail.com>
2021-03-20 23:59:03 -0700

    Removed clear button. Added legend to explain Error Types and Error Status. Moved to one time data filtering by using useEffect.

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -1,7 +1,5 @@
 import React, {useState} from 'react';
 import {ThemeProvider} from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import ClearIcon from '@material-ui/icons/Clear';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container'; //Defines the size of page
 import Toolbar from '@material-ui/core/Toolbar'; 
@@ -11,22 +9,29 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import SearchIcon from '@material-ui/icons/Search';
+import ClearIcon from '@material-ui/icons/Clear';
 import TableContainer from '@material-ui/core/TableContainer';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 import 'fontsource-roboto';
 
 import Checkboxes, {useCheckboxes, getCheckboxName} from './Checkboxes';
 import PTSHelp from './PTSHelp';
+import Legend from './Legend';
 import PTSResults from './PTSResults';
 import * as Constants from '../Constants';
 import {theme} from './theme';
 import "./styles.css";
 import useStyles from "./styles";
-import PTSSignifier from './PTSSignifier';
 
-import {getNotTestedData, getRedirectData, getSearchData, getFailedData, getLatestVersion, mapData} from '../processData';
+
+import {getProfileColor, getSearchData} from '../processData';
 
 
 export function FilterForm(props) {
@@ -34,17 +39,31 @@ export function FilterForm(props) {
     const checkboxes = useCheckboxes();                 // filter to determine which results to display
     const classes = useStyles();                        
 
+
+    const [legend, setLegend] = React.useState(false);
+    
+      const handleLegend = (event) => {
+        //console.log("!!!!legend", legend);
+        setLegend(event.target.checked);
+      };
+
+      const handleClearSearch = (event) => {
+        //console.log("!!!!clear", searchValue);
+        setSearchValue("");
+        event.preventDefault();
+      };
+
     // display the results on the page
     const handleDisplay = (event) => {
         event.preventDefault();
     }
 
     // clear form of all data and reset checkboxes
-    const clearForm = (event) => {
-        checkboxes.checkboxes.map((box, idx) => checkboxes.setCheckbox(idx, false));
-        setSearchValue("");
-        event.preventDefault();
-    }
+    // const clearForm = (event) => {
+    //     checkboxes.checkboxes.map((box, idx) => checkboxes.setCheckbox(idx, false));
+    //     setSearchValue("");
+    //     event.preventDefault();
+    // }
 
     /**
      * Get the data to display. 
@@ -56,79 +75,61 @@ export function FilterForm(props) {
      * 
      */
     const getData = (allData) => {
-        let data = mapData([...allData]);
-        console.log("Mapped Data:", data);
-        let results = data;
+        //let {testProfile : data, dataMap}= mapData([...allData]);
+        let {testProfile : data, dataMap}= allData.all;
+        let {testProfile : latestData, dataMap : latestDataMap}= allData.latest;
 
+        //console.log("Mapped Data:", data, dataMap);
+        
         // determine which checkboxes have been selected
         const filters = checkboxes.checkboxes.filter((checks) => checks.checked === true).map((checkbox) => checkbox.name);
-        console.log("Filters", filters);
+        //console.log("Filters", filters);
         
-        // If no checkboxes have been selected, display nothing
+        // Set the default to display all results
+        let results = data;
+
+        // If no checkboxes have been selected, display 0 results
         if (filters && filters.length === 0 ) {
             results = null;
-        }
-        
-        // If a search value has been defined, get the subset of data that matches the search criteria
-        // and further reduce the data based on the filters selected.
-        if ( searchValue !== "" ) {
-            results = getSearchData(data, searchValue);
-            
-            if (filters && filters.length > 0) {
-                data = results;
-            }
         }
 
         // Top Level Searches
         if (filters.includes(Constants.ALL.name)) 
             results = data;
 
-        if (filters.includes(Constants.LATEST.name))
-            results = getLatestVersion(data);
+        if (filters.includes(Constants.LATEST.name)) {
+            results = latestData;
+            dataMap = latestDataMap;
+            //results = getLatestVersion(data);
+        }
 
         // Secondary Searches
-        if (filters.includes(Constants.ALL.name) || filters.includes(Constants.LATEST.name) )
-            data = results;
-
         //if (filters.includes(Constants.FAILED.name) || filters.includes(Constants.MD5.name) || filters.includes(Constants.SHA.name)) {
-        console.log("filters", filters);
+        //console.log("DataMap before secondary filters", dataMap);
         if (filters.includes(Constants.FAILED.name) && filters.includes(Constants.CHECKSUM.name)) {
-            let newFilter = getAllFilters(Constants.CHECKSUM, filters);
-            //console.log("1 .....");
-            console.log("new filters", newFilter);
-            results = getFailedData(data, newFilter);
+            //let newFilter = getAllFilters(Constants.CHECKSUM, filters);
+            results = dataMap.checksum;
         } else if (filters.includes(Constants.FAILED.name) && filters.includes(Constants.DOWNLOAD.name)) {
-            let newFilter = getAllFilters(Constants.DOWNLOAD, filters);
-            //console.log("2 .....");
-            console.log("new filters", newFilter);
-            results = getFailedData(data, newFilter);
+            //let newFilter = getAllFilters(Constants.DOWNLOAD, filters);
+            results = dataMap.download;
+        } else if (filters.includes(Constants.FAILED.name) && filters.includes(Constants.REDIRECTS.name)) {
+            //let newFilter = getAllFilters(Constants.REDIRECTS, filters);
+            results = dataMap.redirect;
+        } else if (filters.includes(Constants.FAILED.name) && filters.includes(Constants.NONE.name)) {
+            //let newFilter = getAllFilters(Constants.REDIRECTS, filters);
+            results = dataMap.failed;        
+        } else if (filters.includes(Constants.NOT_TESTED.name)) {
+            results = dataMap.notTested;
+        } else if (filters.length <=2 && filters.includes(Constants.FAILED.name) ) {
+            results = dataMap.failed;
         }
-            else if (filters.length <=2 && filters.includes(Constants.FAILED.name) ) {
-            //console.log("3 .....");
-            results = getFailedData(data, filters);
+
+         // If a search value has been defined, get the subset of data that matches the search criteria
+        // and further reduce the data based on the filters selected.
+        if ( searchValue !== "" ) {
+            results = getSearchData(results, searchValue);
         }
-
-        // if (filters.includes(Constants.FAILED.name) && filters.includes(Constants.DOWNLOAD.name)) {
-        //     let newFilter = getAllFilters(Constants.DOWNLOAD, filters);
-        //     console.log("3 .....");
-        //     console.log("new filters", newFilter);
-        //     results = getFailedData(data, newFilter);
-        // } else if (filters.includes(Constants.FAILED.name) ) {
-        //     console.log("4 .....");
-        //     results = getFailedData(data, filters);
-        // }
-
-        if (filters.includes(Constants.REDIRECTS.name))
-            results = getRedirectData(data);
-      
-        if (filters.includes(Constants.NOT_TESTED.name))
-            results = getNotTestedData(data);
-
-        // if (filters.includes(Constants.TIMED_OUT.name))
-        //     results = getTimedOutData(data);
-
-        // if all fails display all the data
-        console.log("++Results", results);
+        
         return results;
     }
 
@@ -153,6 +154,7 @@ export function FilterForm(props) {
         return ch;
     }
 
+    //console.log("!!!!!GETTING DATA");
     let results = getData(props.data);
 
     return(
@@ -160,7 +162,7 @@ export function FilterForm(props) {
             <Container maxWidth="lg" className={classes.root}>
                 <React.Fragment>
                     <form onSubmit={(e) => handleDisplay(e)}>
-                        <div className="App">
+                        <div className={classes.root} className="App">
                             <AppBar>
                                 <Toolbar className={classes.toolbar}>
                                     <Grid justify="space-between"
@@ -182,77 +184,75 @@ export function FilterForm(props) {
                                 </Toolbar>
                             </AppBar>
                         </div>
-                        <Grid container >
-                            <Grid container item  
-                                style={{marginTop:"2em"}}
-                                justify="flex-start"
-                                alignItems="flex-start"
-                                spacing={1} 
-                                direction="row">
-                                <Checkboxes {...checkboxes}  />
-                                    
-                                <Grid item xs={12} sm={3}>
+                        {/* <div className={classes.root}> */}
+                        <Grid  container>
+                            <Grid style={{marginTop:"4rem"}} item xs={12}>
+                                
+                                <Grid container item xs={12}
+                                    // style={{marginTop:"2em"}}
+                                    // justify="flex-start"
+                                    // alignItems="flex-start"
+                                    // spacing={1} 
+                                    // direction="row"
+                                    >
 
-                                    <TextField
-                                        value={searchValue}
-                                        label="Profile Name"
-                                        variant="filled"
-                                        size="small"
-                                        color="primary"
-                                        InputLabelProps={{
-                                            shrink: true,
+                                    <Checkboxes {...checkboxes} displayLegend={legend} />
+                                    <Grid item xs={3}>
+                                        {/* <Paper style={{marginTop:"2em"}} className={classes.paper}>XXXGrid 4-1</Paper> */}
+                                        <TextField
+                                            value={searchValue}
+                                            label="Profile Name"
+                                            variant="filled"
+                                            size="small"
+                                            color="primary"
+                                            InputLabelProps={{
+                                                shrink: true,
+                                                }}
+                                            InputProps={{
+                                                startAdornment: <InputAdornment position="start"><SearchIcon /></InputAdornment>,
+                                                endAdornment: <InputAdornment position="end"><ClearIcon style={{cursor:'default'}} onClick={handleClearSearch}/></InputAdornment>,
+                                                disableUnderline: true,
+                                                className: classes.searchField
+
                                             }}
-                                        InputProps={{
-                                            startAdornment: <InputAdornment position="start"><SearchIcon /></InputAdornment>,
-                                            disableUnderline: true,
-                                            className: classes.searchField
-
-                                        }}
-                                        onChange={(e) => setSearchValue(e.target.value)}   
-                                    />                                 
-                                </Grid>
-                                <Grid item xs={12} sm={3} md={2} lg={2}>
-                                    <Grid container direction="column" >
-                                        <Grid container direction="row" >
-                                            <Grid item><PTSSignifier colorStatus={[1]}/></Grid>
-                                            <Typography>XXX</Typography>
-                                        </Grid>
-                                        <Grid container direction="row" >
-                                            <Grid item><PTSSignifier colorStatus={[3]}/></Grid>
-                                            <Typography>MD5</Typography>
-                                        </Grid>
-                                        <Grid container direction="row" >
-                                            <Grid item><PTSSignifier colorStatus={[4]}/></Grid>
-                                            <Typography>SHA</Typography>
-                                        </Grid>
-
+                                           
+                                            onChange={(e) => setSearchValue(e.target.value)}   
+                                        />      
                                     </Grid>
-                                </Grid>
-                    
-                            </Grid>
-                            
-                            <Grid container 
-                                justify="flex-start"
-                                alignItems="flex-start"
-                                direction="row"> 
-                                <Grid item sm={9} md={8} lg={6} ></Grid>
-                                <Grid item>
-                                    <Button 
-                                        className={classes.button}
-                                        startIcon={<ClearIcon />}
-                                        onClick={(e) => clearForm(e)}>
-                                        Clear
-                                    </Button>
-                                </Grid>
+                                    <Grid item xs={5}>
+                                        {/* <Paper style={{marginTop:"2em"}} className={classes.paper}>XXXGrid 4-2</Paper> */}
+                                        <FormControlLabel
+                                                control={
+                                                <Checkbox
+                                                    checked={legend.checked}
+                                                    onChange={handleLegend}
+                                                    name="legend"
+                                                    color="primary"
+                                                />
+                                                }
+                                                label="Display Legend"
+                                            />
+
+                                             <Grid item container direction="column" >
+                                                <Grid item >  
+                                                    {(legend ? <Legend />: null)}
+                                                </Grid>
+                                            </Grid>
+                                    </Grid>                          
+                                </Grid>                       
                             </Grid>
                         </Grid>
-                                    
                         <Divider style={{marginTop: "1em"}}/>
                     </form>
 
                     {results ? 
-                        (<TableContainer >
-                            <Table className={classes.borders}>
+                        (<TableContainer style={{maxHeight:1000}}>
+                            <Table stickyHeader className={classes.borders}>
+                                <TableHead>
+                                    <TableRow className={classes.tableHeader}>
+                                        <TableCell><Typography className={classes.profileName} color='primary'> Results: {results.length}</Typography></TableCell>
+                                    </TableRow>
+                                </TableHead>
                                 <TableBody className={classes.borders}>
                                     <PTSResults results={results} />
                                 </TableBody>
