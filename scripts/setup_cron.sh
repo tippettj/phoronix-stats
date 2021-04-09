0872e22a3d1a6b8fa7ee07a658fd064efd67abbd
Julie Tippett <julie@phoronix.com>
2021-04-09 05:02:34 +0100

    [setup-cron.sh] fixed error with $HOME env

--- scripts/setup_cron.sh
+++ scripts/setup_cron.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Set up a cron to run every saturday at 2am
-(crontab -l ; echo "0 2 * * 6 ./${HOME}/phoronix-stats/scripts/run-check-tests.sh") | crontab -
+(crontab -l ; echo "0 2 * * 6 ${HOME}/phoronix-stats/scripts/run-check-tests.sh") | crontab 
